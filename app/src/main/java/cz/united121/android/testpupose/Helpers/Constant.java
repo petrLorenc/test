7b090328b4fd4e2c2d28557a2b41be13a2d5f3f9
petrLorenc <lorenc55petr@seznam.cz>
2015-06-15 17:00:10 +0200

    CODE: Preparing for testing broadcast receiver

---
+++ app/src/main/java/cz/united121/android/testpupose/Helpers/Constant.java
@@ -0,0 +1,9 @@
+package cz.united121.android.testpupose.Helpers;
+
+/**
+ * Original project name : TestPupose
+ * Created by Petr Lorenc[petr.lorenc@ackee.cz] on 15.6.2015.
+ */
+public class Constant {
+    public static final String TAG = Constant.class.getName();
+}
